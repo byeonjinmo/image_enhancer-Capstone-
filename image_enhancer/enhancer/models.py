b6b4fb53f10baf70bf152f5cd35c0155adb58d6b
진모 변 <wlsahv@naver.com>
2024-02-29 22:04:24 +0900

    .

--- image_enhancer/enhancer/models.py
+++ image_enhancer/enhancer/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Image(models.Model):
+    title = models.CharField(max_length=255)
+    original_image = models.ImageField(upload_to='images/')
+    enhanced_image = models.ImageField(upload_to='enhanced/', blank=True, null=True)
+    upload_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
