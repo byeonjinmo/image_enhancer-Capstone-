1b931385077333b2dcf0fcf40585a8571c03816e
진모 변 <wlsahv@naver.com>
2024-05-07 21:06:53 +0900

    .

--- image_enhancer/enhancer/views.py
+++ image_enhancer/enhancer/views.py
@@ -8,6 +8,7 @@ from django.shortcuts import get_object_or_404
 from django.shortcuts import render
 
 # GAN 기능 임포트
+
 import torch
 from torchvision.transforms import functional as TF
 
@@ -16,10 +17,13 @@ from .models.dcgan import DCGAN
 from .models.srgan1.model_srgan import GeneratorResNet
 # 이미지 처리 함수
 def enhance_image(image_file, size):
-    # 사용자가 선택한 크기로 이미지 크기를 조정하는 로직
-    size = tuple(map(int, size.split('x')))
-    pil_image = PilImage.open(image_file)
-    pil_image = pil_image.resize(size, PilImage.ANTIALIAS)
+    # 이미지 크기를 조정하는 로직
+    if size == "original":
+        pil_image = PilImage.open(image_file)
+    else:
+        size = tuple(map(int, size.split('x')))
+        pil_image = PilImage.open(image_file)
+        pil_image = pil_image.resize(size, PilImage.ANTIALIAS)
     output_io_stream = io.BytesIO()
     pil_image.save(output_io_stream, format='JPEG', quality=90)
     output_io_stream.seek(0)
@@ -40,8 +44,8 @@ def upload_image_view(request):
         enhanced_image_file = ContentFile(enhanced_image_stream.read(), name="enhanced_" + original_image.name)
         image_instance.enhanced_image.save(enhanced_image_file.name, enhanced_image_file)
 
+        # 이미지 저장 및 상세 페이지로 리디렉션
         image_instance.save()
-
         # 선택된 옵션에 따라 이미지 처리
         enhance_option = request.POST.get('enhance_option')
         if enhance_option == 'gan':
@@ -49,7 +53,7 @@ def upload_image_view(request):
                 self.generator.load_weights(generator_weights)
             # DCGAN 인스턴스 생성
             dcgan_instance = DCGAN()
-            dcgan_instance.load_pretrained_weights('/Users/mac/Desktop/24년 대학/image_enhancer/celebA_5epoch_pth/M_generator_weights.pth')
+            dcgan_instance.load_pretrained_weights('/Users/mac/Desktop/24년 대학/image_enhancer/celebA_5epoch_pth/generator_weights.pth')
 
             # 업로드된 이미지 객체 가져오기
             input_pil_image = PilImage.open(original_image).convert("RGB")
@@ -72,47 +76,59 @@ def upload_image_view(request):
             image_instance.save()
             return redirect('image_detail', image_id=image_instance.id)
 
-        elif enhance_option == 'advanced_gan':
-            # 유료 고급 GAN 처리
-            def load_pretrained_weights(self, generator_weights):
-                self.generator.load_weights(generator_weights)
 
-            # SRGAN 인스턴스 생성
+        elif enhance_option == 'advanced_gan':
+            def load_pretrained_weights(model, weights_path):
+                model.load_state_dict(torch.load(weights_path, map_location=torch.device('cpu')))
+
+                model.eval()
+
+            # 유료 고급 GAN 처리
+
+            iterations = int(request.POST.get('iterations', 100))  # 사용자 입력 반복 횟수
+
             srgan_instance = GeneratorResNet()
 
-            srgan_weights_path = '/Users/mac/Desktop/24년 대학/image_enhancer/celebA_5epoch_pth/generator_weights20.pth'
-            srgan_instance.load_state_dict(torch.load(srgan_weights_path, map_location=torch.device('cpu')))
-            srgan_instance.eval()
+            load_pretrained_weights(srgan_instance, '/Users/mac/Desktop/24년 대학/image_enhancer/celebA_5epoch_pth/generator_weights.pth')
 
-            # 업로드된 이미지를 PIL 이미지로 변환
             input_pil_image = PilImage.open(original_image).convert("RGB")
 
-            # 이미지를 모델에 입력하기 전에 적절한 텐서로 변환
-            input_tensor = TF.to_tensor(input_pil_image).unsqueeze(0)
+            # 반복적 이미지 향상
 
-            # 모델을 사용하여 이미지 개선
-            with torch.no_grad():
-                enhanced_tensor = srgan_instance(input_tensor)
+            for _ in range(iterations):
+                input_tensor = TF.to_tensor(input_pil_image).unsqueeze(0)
 
-            # 모델 출력 후처리
-            enhanced_tensor = (enhanced_tensor.squeeze().detach() + 1) / 2
-            enhanced_pil_image = TF.to_pil_image(enhanced_tensor)
+                with torch.no_grad():
+                    enhanced_tensor = srgan_instance(input_tensor)
 
-            # 개선된 이미지를 바이트 스트림으로 변환 및 저장
-            processed_image_stream = io.BytesIO()
-            enhanced_pil_image.save(processed_image_stream, format='JPEG', quality=90)
-            processed_image_stream.seek(0)
-            processed_image_file = ContentFile(processed_image_stream.read(), name="enhanced_" + original_image.name)
-            image_instance.enhanced_image.save(processed_image_file.name, processed_image_file)
+                enhanced_tensor = (enhanced_tensor.squeeze().detach() + 1) / 2
 
-            # 이미지 인스턴스 저장 및 상세 페이지로 리디렉션
-            image_instance.save()
-            return redirect('image_detail', image_id=image_instance.id)
-            # 이미지 인스턴스 저장 및 상세 페이지로 리디렉션
-        image_instance.save() # 그냥 이미지 조절 상태의 반환
+                input_pil_image = TF.to_pil_image(enhanced_tensor)
+
+            save_enhanced_image(input_pil_image, "enhanced_srgan_" + original_image.name, image_instance)
+
+        # 이미지 인스턴스 저장 및 상세 페이지로 리디렉션
+
         return redirect('image_detail', image_id=image_instance.id)
 
     return render(request, 'enhancer/upload.html')
+
+
+def save_enhanced_image(enhanced_pil_image, filename, image_instance):
+    processed_image_stream = io.BytesIO()
+
+    enhanced_pil_image.save(processed_image_stream, format='JPEG', quality=90)
+
+    processed_image_stream.seek(0)
+
+    processed_image_file = ContentFile(processed_image_stream.read(), name=filename)
+
+    image_instance.enhanced_image.save(processed_image_file.name, processed_image_file)
+
+    image_instance.save()
+
+
+
 # 이미지 상세 보기 뷰
 def image_detail_view(request, image_id):
     image = get_object_or_404(Image, id=image_id)
@@ -121,4 +137,3 @@ def image_detail_view(request, image_id):
 # 시작 화면
 def index_view(request):
     return render(request, 'index.html')
-
