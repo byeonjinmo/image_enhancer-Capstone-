2aa126c61c0858a7be28c5d3544c4bf7d7a3a9d2
진모 변 <wlsahv@naver.com>
2024-05-08 12:57:11 +0900

    .

--- image_enhancer/enhancer/models/srgan1/srgan.py
+++ image_enhancer/enhancer/models/srgan1/srgan.py
@@ -45,7 +45,7 @@ parser.add_argument("--decay_epoch", type=int, default=100, help="epoch from whi
 parser.add_argument("--n_cpu", type=int, default=8, help="number of cpu threads to use during batch generation")
 parser.add_argument("--hr_height", type=int, default=256, help="high res. image height")
 parser.add_argument("--hr_width", type=int, default=256, help="high res. image width")
-parser.add_argument("--channels", type=int, default=3, help="number of image channels")
+parser.add_argument("--channels", type=int, default=1, help="number of image channels")
 parser.add_argument("--sample_interval", type=int, default=100, help="interval between saving image samples")
 parser.add_argument("--checkpoint_interval", type=int, default=-1, help="interval between model checkpoints")
 opt = parser.parse_args()
